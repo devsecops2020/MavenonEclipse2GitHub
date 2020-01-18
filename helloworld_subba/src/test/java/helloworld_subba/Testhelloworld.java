cff508d83e8352ecf4e1c28e20aaf34edbbf4bfe
devsecops2020 <devsecops2020@gmail.com>
2020-01-18 15:45:36 +0530

    First Commit

---
+++ helloworld_subba/src/test/java/helloworld_subba/Testhelloworld.java
@@ -0,0 +1,13 @@
+package helloworld_subba;
+
+import org.testng.annotations.Test;
+
+public class Testhelloworld {
+
+	
+	@Test
+	public void Testhelloworld() {
+		
+		System.out.println("..Hello World...!");
+	}
+}
